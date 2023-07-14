9ac490c57f09814ecfb9498920956a7f0199db88
shashank shakya <58992351+shashank3103-dev@users.noreply.github.com>
2023-07-14 12:59:22 +0530

    master

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,13 @@
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
